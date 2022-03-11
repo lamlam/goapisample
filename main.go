0eb82f0fc0ce92f45ad29a636960101bbe278449
lamlam <lamlam4r@gmail.com>
2022-03-11 21:30:38 +0900

    Add echo

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 )
 
-func sample(w http.ResponseWriter, res *http.Request) {
+func echo(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("request to echo")
+	body := req.Body
+	defer body.Close()
+
+	scanner := bufio.NewScanner(req.Body)
+	for scanner.Scan() {
+		fmt.Fprint(w, scanner.Text())
+	}
+}
+
+func sample(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("request to sample")
 	fmt.Fprintf(w, "test")
 }
 
 func main() {
 	port := "8080"
 	http.HandleFunc("/sample", sample)
+	http.HandleFunc("/echo", echo)
 
 	fmt.Printf("Start server at localhost:%s\n", port)
 	http.ListenAndServe(":"+port, nil)

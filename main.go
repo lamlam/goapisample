2644bc22a7920331a43b8b02cf8626f846de49df
lamlam <lamlam4r@gmail.com>
2022-03-11 22:06:47 +0900

    Add random2

--- main.go
+++ main.go
@@ -2,9 +2,56 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 )
+
+type successMessage struct {
+	Message int `json:"success"`
+}
+type failMessage struct {
+	Message int `json:"fail"`
+}
+type random2Response struct {
+	Results []interface{} `json:"results"`
+}
+
+// 10回randして下記メッセージの配列を返す
+// - 50以上なら {message: "70"}
+// - 50未満なら {fail: "31"}
+func random2(w http.ResponseWriter, req *http.Request) {
+	threshold := 50
+	results := make([]interface{}, 10)
+	for i := range results {
+		v := rand.Intn(100)
+		if v >= threshold {
+			results[i] = successMessage{v}
+		} else {
+			results[i] = failMessage{v}
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(random2Response{results})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+type randomResponse struct {
+	Value int `json:"value"`
+}
+
+func random(w http.ResponseWriter, req *http.Request) {
+	resp := randomResponse{rand.Intn(100)}
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
 
 func echo(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("request to echo")
@@ -26,6 +73,8 @@ func main() {
 	port := "8080"
 	http.HandleFunc("/sample", sample)
 	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/random", random)
+	http.HandleFunc("/random2", random2)
 
 	fmt.Printf("Start server at localhost:%s\n", port)
 	http.ListenAndServe(":"+port, nil)
